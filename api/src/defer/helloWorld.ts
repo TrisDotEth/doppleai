94668b497d970966cdf51e6f45df741d30dbb92c
Tris <tris@hivemind.me>
2023-03-20 11:29:53 +1100

    defer testing

--- api/src/defer/helloWorld.ts
+++ api/src/defer/helloWorld.ts
@@ -3,12 +3,12 @@ import { defer } from '@defer/client'
 import { createThought } from 'src/services/thought/thought'
 
 // a background function must be `async`
-async function helloWorld(input) {
-  console.log('helloWorld in background input value:', input)
-  const latestThought = await createThought({ input })
-  console.log('latestThought in background', latestThought)
-  return latestThought
+const helloWorld = async () {
+  console.log('helloWorld in background input value:')
+  // const latestThought = await createThought({ input })
+  // console.log('latestThought in background', latestThought)
+  // return latestThought
 }
 
 // the function must be wrapped with `defer()` and exported as default
-export default defer(helloWorld)
+export default defer.cron(helloWorld, '0 * * * *')
